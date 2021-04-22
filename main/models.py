9521edb52fa2edda94eff3789203eb81664d80bb
realibi <reallibi@gmail.com>
2021-04-22 10:37:57 +0600

    templae structure added

---
+++ main/models.py
@@ -0,0 +1,4 @@
+from django.db import models
+
+# Create your models here.
+#image = models.ImageField(upload_to='club_images', blank=True, null=True)
---
+++ main/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from django.conf.urls import url, include
+
+from . import views
+
+app_name= "main"
+urlpatterns = [
+    path('', views.index, name='index'), 
+]
+#
