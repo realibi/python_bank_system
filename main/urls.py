54784126a9759f2b1031291dd00b63aec9b35c1b
realibi <reallibi@gmail.com>
2021-05-28 03:26:39 +0600

    money transfer and payments added

--- main/urls.py
+++ main/urls.py
@@ -7,5 +7,7 @@ app_name= "main"
 urlpatterns = [
     path('', views.index, name='index'),
     path('registration/', views.registration, name='registration'),
+    path('login/', views.login, name='login'),
+    path('transfer/', views.money_transfer, name='money_transfer'),
+    path('payments/', views.service_payment, name='service_payment'),
 ]
-#
--- main/views.py
+++ main/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, reverse
 from django.http import HttpResponse
 from .models import Customer, Account
 
@@ -27,3 +27,74 @@ def registration(request):
         
     else:
         return HttpResponse("handling get")
+
+
+def login(request):
+    if request.method == "POST":
+        login = request.POST.get("login", None)
+        password = request.POST.get("password", None)
+        try:
+            customer = Customer.objects.get(customer_login=login, customer_password=password)
+            print("trying to find customer")
+            if customer:
+                print("customer not null")
+                account = Account.objects.filter(customer=customer)[0]
+                request.session["user_id"] = account.id
+                request.session["user_number"] = account.number
+                return redirect(reverse('main:index'))
+        except:
+            print("something went wrong")
+            return render(request, "login.html", {})
+    else:
+        return render(request, "login.html", {})
+
+
+def money_transfer(request):
+    print("money transfer is starting")
+    if request.method == "POST":
+        current_account_id = int(request.session["user_id"])
+        print(current_account_id)
+        receiver_number = request.POST.get("receiver_number", None)
+        amount = int(request.POST.get("amount", None))
+        try:
+            sender_account = Account.objects.get(id=current_account_id)
+            receiver_account = Account.objects.get(number=receiver_number)
+            if receiver_account:
+                if sender_account.balance >= amount:
+                    sender_account.balance = sender_account.balance - amount
+                    print(sender_account.balance)
+                    sender_account.save()
+
+                    print("receiver_account found!")
+                    receiver_account.balance = receiver_account.balance + amount
+                    
+                
+                    receiver_account.save()
+                    return redirect(reverse('main:index'))
+        except:
+            print("something went wrong")
+            return render(request, "login.html", {})
+    else:
+        return render(request, "transfer.html", {})
+
+
+def service_payment(request):
+    print("service payment is starting")
+    if request.method == "POST":
+        current_account_id = int(request.session["user_id"])
+        amount = int(request.POST.get("amount", None))
+        try:
+            sender_account = Account.objects.get(pk=current_account_id)
+            print(sender_account)
+            
+            if sender_account.balance >= amount:
+                sender_account.balance = sender_account.balance - amount
+                print(sender_account.balance)
+                sender_account.save()
+        
+                return redirect(reverse('main:index'))
+        except:
+            print("something went wrong")
+            return render(request, "login.html", {})
+    else:
+        return render(request, "payments.html", {})
